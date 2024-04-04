ec32f91201c16bae9a440082913742a94000a2b3
c.damasceno <c.damasceno@reply.com>
2024-04-04 15:23:32 -0300

    first commit

---
+++ src/main/java/com/processo/horangebloodheroes/HorangebloodheroesApplication.java
@@ -0,0 +1,13 @@
+package com.processo.horangebloodheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorangebloodheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorangebloodheroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/processo/horangebloodheroes/HorangebloodheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processo.horangebloodheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorangebloodheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
